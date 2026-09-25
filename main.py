3f8a8fbe007ffc7f728477f6ab163947d8156840
Joao Marcos <joao-marcos20@protonmail.com>
2026-09-25 16:47:06 -0300

    Add exceptions handling

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ load_dotenv()
 host = os.getenv("SSH_HOST")
 user = os.getenv("SSH_USER")
 
-command = ["ssh", user + "@" + host, "echo hello"]
-result = subprocess.run(command, capture_output=True, text=True)
+try: 
+    command = ["ssh", user + "@" + host, "echo hello"]
+    result = subprocess.run(command, capture_output=True, text=True, timeout=4)
 
-if result.returncode == 0:
-    print ("Success")
-    print (result.stdout)
-else:
-    print ("Command failed")
-    print (result.stderr)
+    if result.returncode == 0:
+        print ("Success")
+        print (result.stdout)
+    else:
+        print ("Command failed")
+        print (result.stderr)
+
+except subprocess.TimeoutExpired:
+    print("Command timed out")
+
+except Exception:
+    print("Something else went wrong")
 
